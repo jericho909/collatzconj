7046215457adef9cd75934e08731101d4a04c486
jericho909 <omerahmetyucel@gmail.com>
2023-06-27 21:22:32 +0300

    Added functionaliy to store previous calculations

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use std::io::{self, Read};
+use std::io::{self};
+use std::collections::HashMap;
 
 fn main() {
     println!("Hello, this is a program that uses the Collatz conjecture.");
     println!("The conjecture asks whether repeating two simple arithmetic operations will eventually transform every positive integer into 1.");
     println!("Let's see, shall we?");
+
+    //create a hash map to store the results
+    let mut results = HashMap::new();
 
     loop {
         println!("Please enter a positive integer:");
@@ -29,10 +33,12 @@ fn main() {
             break;
         }
 
+        let number_original = number.clone();
+
         println!("Beginning the calculations.");
 
         let mut counter: i32 = 0;
-
+        
         // Begin the main calcution loop
         while number != 1 {
             if number % 2 == 0 {
@@ -44,15 +50,19 @@ fn main() {
                 println!("The number is odd, tripling it and adding one.");
                 println!("{:8} -->", number);
             }
-
+            
             counter += 1;
         }
 
         println!("The calculations took {} steps.", counter);
+        
+        results.insert(number_original, counter);
+        
+
 
         //loop to check the state of the program
         loop {
-            println!("Please enter 'q' to quit or 'c' to continue:");
+            println!("Please enter 'q' to quit, 'c' to continue or 'r' for past results:");
 
             let mut input_2 = String::new();
             io::stdin()
@@ -70,11 +80,22 @@ fn main() {
                     println!("Continuing to the next iteration.");
                     break;
                 }
+                "r" => {
+                    show_results(&results);
+                }
+                
                 _ => {
                     println!("Invalid input. Please try again.");
                     println!("(Did you enter something other than 'q' or 'c' ?)");
                 }
             }
         }
+    }
+
+}
+
+fn show_results (results: &HashMap<i64, i32>) {
+    for (k, v) in results {
+        println!("The number {k} took {v} calculations to reach 1 and therefore did not disprove the Collatz Conjecture.")
     }
 }
